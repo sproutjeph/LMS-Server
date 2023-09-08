2f73e67bac665e6bd819936d44dbd4594ab334c8
jephthah <jephthah.mbah@outlook.com>
2023-09-08 15:50:04 +0100

    added answer route

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -6,6 +6,9 @@ import { createCourse } from "../../services/course.service";
 import CourseModel from "../model/course.modal";
 import { redis } from "../utils/redis";
 import mongoose from "mongoose";
+import path from "path";
+import ejs from "ejs";
+import sendEmail from "../utils/sendMail";
 
 export const uploadCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -196,6 +199,86 @@ export const addQuestion = CatchAsyncError(
 
       //save the course
       await course?.save();
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      throw new BadRequestError(`${error.message}`);
+    }
+  }
+);
+
+// add answer in course question
+
+interface IAddAnswerData {
+  answer: string;
+  courseId: string;
+  contentId: string;
+  questionId: string;
+}
+
+export const addAnswer = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { answer, courseId, contentId, questionId }: IAddAnswerData =
+        req.body;
+
+      const course = await CourseModel.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        throw new BadRequestError("Invalid content id");
+      }
+
+      const courseContent = course?.courseData?.find((item: any) =>
+        item._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        throw new BadRequestError("Invalid content id");
+      }
+      const question = courseContent?.questions?.find((item: any) =>
+        item._id.equals(questionId)
+      );
+
+      if (!question) {
+        throw new BadRequestError("Invalid question Id");
+      }
+      //create a new answer
+      const newAnswer: any = {
+        user: req.user,
+        answer,
+      };
+
+      //add this answer to our course content
+      question.questionReplies?.push(newAnswer);
+
+      await course?.save();
+
+      if (req.user?._id === question.user._id) {
+        //create a notification
+      } else {
+        const data = {
+          name: question.user.name,
+          title: courseContent.title,
+        };
+        const html = await ejs.renderFile(
+          path.join(__dirname, "../mails/question-reply.ejs"),
+          data
+        );
+
+        try {
+          await sendEmail({
+            email: question.user.email,
+            subject: "Question replay",
+            template: "question-replay.ejs",
+            data,
+          });
+        } catch (error: any) {
+          throw new BadRequestError(`${error.message}`);
+        }
+      }
 
       res.status(200).json({
         success: true,
--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
+  addAnswer,
   addQuestion,
   editCourse,
   getAllCourses,
@@ -27,5 +28,6 @@ router.get("/get-course/:id", getSingleCourse);
 router.get("/get-courses", getAllCourses);
 router.get("/get-course-content/:id", isAuthenticated, getCoursesByUser);
 router.put("/add-question", isAuthenticated, addQuestion);
+router.put("/add-answer", isAuthenticated, addAnswer);
 
 export default router;
